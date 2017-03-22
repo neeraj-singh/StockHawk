f015c73cfa3bfdbad36855277b7f98b01a8e689b
neeraj-singh <neeraj2raj@gmail.com>
2017-03-23 00:06:46 +0530

    soem change

---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailActivity.java
@@ -0,0 +1,204 @@
+package com.udacity.stockhawk.ui;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private static final int PAGE_LIMIT = 2;
+    private static int LOADER_ID = 0;
+    public Boolean dataLoaded = false;
+    @BindView(R.id.toolbar)
+    public Toolbar toolbar;
+    @BindView(R.id.stock_name)
+    public TextView tvStockName;
+//    @BindView(R.id.stock_exchange)
+//    public TextView tvStockExchange;
+    @BindView(R.id.stock_price)
+    public TextView tvStockPrice;
+    @BindView(R.id.day_highest)
+    public TextView tvDayHighest;
+    @BindView(R.id.day_lowest)
+    public TextView tvDayLowest;
+    @BindView(R.id.absolute_change)
+    public TextView tvAbsoluteChange;
+    @BindView(R.id.viewpager)
+    public ViewPager viewPager;
+    @BindView(R.id.tabs)
+    public TabLayout tabLayout;
+    private Uri stockUri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        supportPostponeEnterTransition();
+        setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        stockUri = intent == null ? null : intent.getData();
+
+        setupViewPager();
+
+        getSupportLoaderManager().initLoader(LOADER_ID, null, this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager() {
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        Bundle bundle = new Bundle();
+        DetailFragment monthlyStock = new DetailFragment();
+        bundle.putString(getString(R.string.FRAGMENT_DATA_TYPE_KEY), getString(R.string.MONTHLY));
+        monthlyStock.setArguments(bundle);
+
+        DetailFragment weeklyStock = new DetailFragment();
+        bundle = new Bundle();
+        bundle.putString(getString(R.string.FRAGMENT_DATA_TYPE_KEY), getString(R.string.WEEKLY));
+        weeklyStock.setArguments(bundle);
+
+        DetailFragment dailyStock = new DetailFragment();
+        bundle = new Bundle();
+        bundle.putString(getString(R.string.FRAGMENT_DATA_TYPE_KEY), getString(R.string.DAILY));
+        dailyStock.setArguments(bundle);
+
+        viewPagerAdapter.addFragment(dailyStock, getString(R.string.days_fragment_title));
+        viewPagerAdapter.addFragment(weeklyStock, getString(R.string.weeks_fragment_title));
+        viewPagerAdapter.addFragment(monthlyStock, getString(R.string.months_fragment_title));
+
+        viewPager.setAdapter(viewPagerAdapter);
+        viewPager.setOffscreenPageLimit(PAGE_LIMIT);
+        tabLayout.setupWithViewPager(viewPager, true);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (stockUri != null) {
+            return new CursorLoader(
+                    this,
+                    stockUri,
+                    Contract.Quote.QUOTE_COLUMNS.toArray(new String[]{}),
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (data.moveToFirst()) {
+            String stockExchange = data.getString(Contract.Quote.POSITION_EXCHANGE);
+            String stockName = data.getString(Contract.Quote.POSITION_NAME);
+            Float stockPrice = data.getFloat(Contract.Quote.POSITION_PRICE);
+            Float absolutionChange = data.getFloat(Contract.Quote.POSITION_ABSOLUTE_CHANGE);
+            Float dayLowest = data.getFloat(Contract.Quote.POSITION_LOWEST);
+            Float dayHighest = data.getFloat(Contract.Quote.POSITION_HIGHEST);
+
+            getWindow().getDecorView().setContentDescription(
+                    String.format(getString(R.string.detail_activity_cd), stockName));
+
+            DecimalFormat dollarFormat = (DecimalFormat) NumberFormat.getCurrencyInstance(Locale.getDefault());
+            dollarFormat.setMaximumFractionDigits(2);
+            dollarFormat.setMinimumFractionDigits(2);
+
+//            tvStockExchange.setText(stockExchange);
+            tvStockName.setText(stockName);
+            tvStockPrice.setText(dollarFormat.format(stockPrice));
+            tvStockPrice.setContentDescription(String.format(getString(R.string.stock_price_cd), tvStockPrice.getText()));
+            tvAbsoluteChange.setText(dollarFormat.format(absolutionChange));
+            if (dayHighest != -1) {
+                tvDayHighest.setText(dollarFormat.format(dayHighest));
+                tvDayHighest.setContentDescription(String.format(getString(R.string.day_highest_cd), tvDayHighest.getText()));
+                tvDayLowest.setText(dollarFormat.format(dayLowest));
+                tvDayLowest.setContentDescription(String.format(getString(R.string.day_lowest_cd), tvDayLowest.getText()));
+            } else {
+                tvDayLowest.setVisibility(View.GONE);
+                tvDayHighest.setVisibility(View.GONE);
+            }
+            if (absolutionChange > 0) {
+                tvAbsoluteChange.setBackgroundResource(R.drawable.percent_change_pill_green);
+                tvAbsoluteChange.setContentDescription(
+                        String.format(getString(R.string.stock_increment_cd), tvAbsoluteChange.getText()));
+            } else {
+                tvAbsoluteChange.setBackgroundResource(R.drawable.percent_change_pill_red);
+                tvAbsoluteChange.setContentDescription(
+                        String.format(getString(R.string.stock_decrement_cd), tvAbsoluteChange.getText()));
+            }
+        }
+    }
+
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        dataLoaded = false;
+    }
+
+    public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+        private final List<Fragment> fragmentList = new ArrayList<>();
+        private final List<String> fragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return fragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragmentList.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return fragmentTitleList.get(position);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            fragmentList.add(fragment);
+            fragmentTitleList.add(title);
+        }
+    }
+}
---
+++ app/src/main/java/com/udacity/stockhawk/ui/DetailFragment.java
@@ -0,0 +1,194 @@
+
+/**
+ * Created by neeraj.singh on 22/03/17.
+ */
+
+package com.udacity.stockhawk.ui;
+
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.animation.Easing;
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.Description;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.data.Contract;
+import com.udacity.stockhawk.utils.CustomMarkerView;
+import com.udacity.stockhawk.utils.DateParser;
+import com.udacity.stockhawk.utils.XAxisFormatter;
+import com.udacity.stockhawk.utils.YAxisFormatter;
+
+import java.util.List;
+
+import butterknife.BindColor;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    public String fragmentDataType;
+    public String dateFormat;
+    public int dataColumnPosition;
+    public Uri stockUri;
+    public String historyData;
+    public int LOADER_ID;
+    @BindView(R.id.chart)
+    public LineChart linechart;
+    @BindColor(R.color.white)
+    public int white;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+        if (savedInstanceState == null) {
+            fragmentDataType = getArguments().getString(getString(R.string.FRAGMENT_DATA_TYPE_KEY));
+            if (fragmentDataType.equals(getString(R.string.MONTHLY))) {
+                dataColumnPosition = Contract.Quote.POSITION_MONTH_HISTORY;
+                dateFormat = "MMM";
+                LOADER_ID = 100;
+            } else if (fragmentDataType.equals(getString(R.string.WEEKLY))) {
+                dataColumnPosition = Contract.Quote.POSITION_WEEK_HISTORY;
+                dateFormat = "dd";
+                LOADER_ID = 200;
+            } else {
+                dataColumnPosition = Contract.Quote.POSITION_DAY_HISTORY;
+                dateFormat = "dd";
+                LOADER_ID = 300;
+            }
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+        ButterKnife.bind(this, rootView);
+        if (historyData != null) {
+            setUpLineChart();
+        }
+        return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        stockUri = getActivity().getIntent().getData();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getLoaderManager().initLoader(LOADER_ID, null, this);
+    }
+
+    private Entry getLastButOneData(List<Entry> dataPairs) {
+        if (dataPairs.size() > 2) {
+            return dataPairs.get(dataPairs.size() - 2);
+        } else {
+            return dataPairs.get(dataPairs.size() - 1);
+        }
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        if (stockUri != null && getContext() != null) {
+            return new CursorLoader(
+                    getContext(),
+                    stockUri,
+                    Contract.Quote.QUOTE_COLUMNS.toArray(new String[]{}),
+                    null,
+                    null,
+                    null
+            );
+        }
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        if (historyData == null && data.moveToFirst()) {
+            //set up the chart with history data
+            historyData = data.getString(dataColumnPosition);
+            setUpLineChart();
+            getActivity().supportStartPostponedEnterTransition();
+        }
+    }
+
+    private void setUpLineChart() {
+        Pair<Float, List<Entry>> result = DateParser.getFormattedStockHistory(historyData);
+        List<Entry> dataPairs = result.second;
+        Float referenceTime = result.first;
+        LineDataSet dataSet = new LineDataSet(dataPairs, "");
+        dataSet.setColor(white);
+        dataSet.setLineWidth(2f);
+        dataSet.setDrawHighlightIndicators(false);
+        dataSet.setCircleColor(white);
+        dataSet.setHighLightColor(white);
+        dataSet.setDrawValues(false);
+
+        LineData lineData = new LineData(dataSet);
+        linechart.setData(lineData);
+
+        XAxis xAxis = linechart.getXAxis();
+        xAxis.setValueFormatter(new XAxisFormatter(dateFormat, referenceTime));
+        xAxis.setDrawGridLines(false);
+        xAxis.setAxisLineColor(white);
+        xAxis.setAxisLineWidth(1.5f);
+        xAxis.setTextColor(white);
+        xAxis.setTextSize(12f);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+
+        YAxis yAxisRight = linechart.getAxisRight();
+        yAxisRight.setEnabled(false);
+
+        YAxis yAxis = linechart.getAxisLeft();
+        yAxis.setValueFormatter(new YAxisFormatter());
+        yAxis.setDrawGridLines(false);
+        yAxis.setAxisLineColor(white);
+        yAxis.setAxisLineWidth(1.5f);
+        yAxis.setTextColor(white);
+        yAxis.setTextSize(12f);
+
+        CustomMarkerView customMarkerView = new CustomMarkerView(getContext(),
+                R.layout.marker_view, getLastButOneData(dataPairs), referenceTime);
+
+
+        Legend legend = linechart.getLegend();
+        legend.setEnabled(false);
+
+        linechart.setMarker(customMarkerView);
+
+        linechart.setDragEnabled(false);
+        linechart.setScaleEnabled(false);
+        linechart.setDragDecelerationEnabled(false);
+        linechart.setPinchZoom(false);
+        linechart.setDoubleTapToZoomEnabled(false);
+        Description description = new Description();
+        description.setText(" ");
+        linechart.setDescription(description);
+        linechart.setExtraOffsets(10, 0, 0, 10);
+        linechart.animateX(1500, Easing.EasingOption.Linear);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        //No action needed
+    }
+}
